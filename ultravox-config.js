f8d7f123a174bf76aa1e0d75f05df577d9f847a7
gulzaib002 <gulzaib.dev@gmail.com>
2025-05-15 20:55:05 +0500

    fixed env issues

--- ultravox-config.js
+++ ultravox-config.js
@@ -1,5 +1,3 @@
-const toolsBaseUrl = "https://5652-2400-adc5-494-a500-292d-1034-d710-c914.ngrok-free.app";// TODO ngrok URL here
-
 
 export let ULTRAVOX_CALL_CONFIG = {
     medium: { "twilio": {} },
--- ultravox-utils.js
+++ ultravox-utils.js
@@ -1,38 +1,29 @@
-import 'dotenv/config';
 import https from 'node:https';
-
-// Configuration
-const ULTRAVOX_API_KEY = process.env.ULTRAVOX_API_KEY;
-const ULTRAVOX_API_URL = 'https://api.ultravox.ai/api';
 
 
 // Create Ultravox call and get join URL
 export async function createUltravoxCall(callConfig, agentID) {
- 
-
-    const request = https.request(`${ULTRAVOX_API_URL}/agents/${agentID}/calls`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-API-Key': ULTRAVOX_API_KEY
-        }
-    });
-
+    const ULTRAVOX_API_KEY = process.env.ULTRAVOX_API_KEY;
   
-
-    return new Promise((resolve, reject) => {
-        let data = '';
-
-        request.on('response', (response) => {
-         
-
-            response.on('data', chunk => data += chunk);
-            response.on('end', () => resolve(JSON.parse(data)));
-        });
-
-        request.on('error', reject);
-        request.write(JSON.stringify(callConfig));
-        request.end();
+    const request = https.request(`https://api.ultravox.ai/api/agents/${agentID}/calls`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-Key': ULTRAVOX_API_KEY
+      }
     });
-}
-
+  
+    return new Promise((resolve, reject) => {
+      let data = '';
+  
+      request.on('response', (response) => {
+        response.on('data', chunk => data += chunk);
+        response.on('end', () => resolve(JSON.parse(data)));
+      });
+  
+      request.on('error', reject);
+      request.write(JSON.stringify(callConfig));
+      request.end();
+    });
+  }
+  
